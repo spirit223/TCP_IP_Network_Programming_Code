130f36a06535817c7f9bfa59aca0de1259b3fb29
小吴来哩 <2235569493@qq.com>
2024-01-20 01:58:17 +0800

    finished code

--- hello_server.c
+++ hello_server.c
@@ -26,7 +26,40 @@ int main(int argc, char* argv[]) {
   
   serv_sock = socket(PF_INET, SOCK_STREAM, 0);
 
-  
+  if (serv_sock == -1)
+  {
+    error_handling("socket() error");
+  }
 
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family=AF_INET;
+  serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+  serv_addr.sin_port=htons(atoi(argv[1]));
+
+  if (bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
+  {
+    error_handling("bind() error");
+  }
+
+  if (listen(serv_sock, 5) == -1)
+  {
+    error_handling("listen() error");
+  }
+  
+  clnt_addr_size=sizeof(clnt_addr);
+  clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+  if (clnt_sock == -1)
+  {
+    error_handling("accept() error");
+  }
+  write(clnt_sock, message, sizeof(message));
+  close(clnt_sock);
+  close(serv_sock);
   return 0;
+}
+
+void error_handling(char* message) {
+  fputs(message, stderr);
+  fputc('/n', stderr);
+  exit(1);
 }
